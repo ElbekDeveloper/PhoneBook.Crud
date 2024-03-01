b67a521433490a41bb10990b5bed448c3592b637
ElbekDeveloper <normurodovelbek0133@gmail.com>
2024-03-01 21:07:44 +0300

    Implement Logging

---
+++ PhoneBook.Crud/Brokers/ILoggingBroker.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PhoneBook.Crud.Brokers
+{
+    internal interface ILoggingBroker
+    {
+        void LogInformation(string message);
+        void LogError(Exception exception);
+    }
+}
