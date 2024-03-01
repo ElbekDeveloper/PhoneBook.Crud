9b183f002871f19d52806b761e6c87a6c9f57978
YaqubAliy02 <91617731+YaqubAliy02@users.noreply.github.com>
2024-03-01 11:22:07 +0500

    Create Contact

---
+++ PhoneBook.Crud/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace PhoneBook.Crud.Models
+{
+    internal class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
